beca7b061b8390b533ba791d702fceb42754c23b
Niel Mistry <nielmistry1@gmail.com>
2019-07-17 20:44:56 -0400

    Cleanup

--- GUI_Images/Untitled.c
+++
@@ -1,9 +0,0 @@
-/* GIMP RGB C-Source image dump (Untitled.c) */
-
-#define GIMP_IMAGE_WIDTH (1)
-#define GIMP_IMAGE_HEIGHT (1)
-#define GIMP_IMAGE_BYTES_PER_PIXEL (3) /* 3:RGB, 4:RGBA */
-#define GIMP_IMAGE_PIXEL_DATA ((unsigned char*) GIMP_IMAGE_pixel_data)
-static const unsigned char GIMP_IMAGE_pixel_data[1 * 1 * 3 + 1] =
-("\20\4");
-
